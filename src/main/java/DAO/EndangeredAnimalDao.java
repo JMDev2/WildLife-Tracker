2a44112bb794cc68706ad795de7f36b3443dd0be
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-05-07 21:53:48 +0300

    This will add the CRUDE methods in the dao

--- src/main/java/DAO/EndangeredAnimalDao.java
+++ src/main/java/DAO/EndangeredAnimalDao.java
@@ -11,7 +11,7 @@ import java.util.List;
 import static models.DB.sql2o;
 
 public class EndangeredAnimalDao implements EndangeredAnimalManagement{
-
+        //CREATE
     @Override
     public void add(EndangeredAnimal endangeredAnimal) {
         try(Connection con = sql2o.open()){
@@ -22,21 +22,25 @@ public class EndangeredAnimalDao implements EndangeredAnimalManagement{
                     .addParameter("age", endangeredAnimal.getAge())
                     .executeUpdate()
                     .getKey();
-
-
         }
-
     }
-
+        //UPDATE
     @Override
     public void update(EndangeredAnimal endangeredAnimal) {
-
+        try(Connection con = sql2o.open()){
+            String sql = "UPDATE animals SET name = :name WHERE id = :id";
+            con.createQuery(sql)
+                    .addParameter("id", endangeredAnimal.id)
+                    .addParameter("name", endangeredAnimal.name)
+                    .executeUpdate();
+        }
     }
 
     @Override
     public EndangeredAnimal EndangeredAnimal(int id) {
         return null;
     }
+            //READ
 
     @Override
     public List<EndangeredAnimal> getEndangeredAnimal() {
@@ -46,9 +50,16 @@ public class EndangeredAnimalDao implements EndangeredAnimalManagement{
         }
     }
 
-
+        //DELETE
     @Override
     public void deleteEndangeredAnimal(int id) {
+        try (Connection con = sql2o.open()){
+            String sql = "DELETE FROM animals WHERE id =:id";
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
+
 
     }
 
--- src/test/java/models/EndangeredAnimalTest.java
+++ src/test/java/models/EndangeredAnimalTest.java
@@ -4,11 +4,17 @@ import DAO.EndangeredAnimalDao;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.Arrays;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EndangeredAnimalTest {
     @Rule
     public DatabaseRule database = new DatabaseRule();
+
+
 
     @Test
     public void save_insertsObjectIntoDatabase_true() {
@@ -17,5 +23,17 @@ public class EndangeredAnimalTest {
         endangeredAnimalDao.add(endangeredAnimal);
         assertTrue(endangeredAnimalDao.getEndangeredAnimal().get(0).equals(endangeredAnimal));
     }
+//    @Test
+//    public void getMonsters_retrievesAllMonstersFromDatabase_monstersList() {
+//        EndangeredAnimal endangeredAnimal = new EndangeredAnimal("Rhino", EndangeredAnimal.HEALTHY, EndangeredAnimal.YOUNG);
+//        EndangeredAnimalDao endangeredAnimalDao = new EndangeredAnimalDao();
+//        endangeredAnimalDao.add(endangeredAnimal);
+//        endangeredAnimal.add();
+//        Monster firstMonster = new Monster("Bubbles", testPerson.getId());
+//        firstMonster.save();
+//        Monster secondMonster = new Monster("Spud", testPerson.getId());
+//        secondMonster.save();
+//        Monster[] monsters = new Monster[] { firstMonster, secondMonster };
+//        assertTrue(testPerson.getMonsters().containsAll(Arrays.asList(monsters)));
 
 }
