06d35cc2d5da68bb1012bc4ad67f0eb669e7b348
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-05-08 20:46:39 +0300

    This will add the new sighting file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,11 @@
+package models;
+
+import models.Animal;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.*;
@@ -14,6 +18,25 @@ public class App {
             Map<String, Object> model = new HashMap<String, Object>();
             String heroName = request.queryParams("heroName");
             return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/newanimal", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String heroName = request.queryParams("heroName");
+            return new ModelAndView(model, "newanimal.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/animals", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String name = request.queryParams("name");
+            try {
+                Animal animal = new Animal(name);
+                animal.add();
+            } catch (IllegalArgumentException exception) {
+                System.out.println("Please enter an animal name.");
+            }
+            response.redirect("/animals");
+            return new ModelAndView(model, layout);
         }, new HandlebarsTemplateEngine());
     }
 }
