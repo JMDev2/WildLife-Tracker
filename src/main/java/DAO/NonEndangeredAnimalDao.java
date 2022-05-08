d292b9757568092b070e5d9ea20d278d6df81de1
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-05-08 09:10:22 +0300

    This will create the sighting classes

--- src/main/java/DAO/NonEndangeredAnimalDao.java
+++ src/main/java/DAO/NonEndangeredAnimalDao.java
@@ -1,33 +1,69 @@
 package DAO;
 
+import models.DB;
+import models.EndangeredAnimal;
 import models.NonEndangeredAnimal;
+import org.sql2o.Connection;
 
 import java.util.List;
 
-public class NonEndangeredAnimalDao implements NonEndangeredAnimalManagement {
+import static models.DB.sql2o;
 
+public class NonEndangeredAnimalDao implements NonEndangeredAnimalManagement {
+        //CREATE
     @Override
     public void add(NonEndangeredAnimal nonEndangeredAnimal) {
-
+        try(Connection con = sql2o.open()) {
+            String sql = "INSERT INTO animals(name) VALUES (:name )";
+            nonEndangeredAnimal.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", nonEndangeredAnimal.name)
+                    .executeUpdate()
+                    .getKey();
+        }
     }
 
+    //UPDATE
     @Override
     public void update(NonEndangeredAnimal nonEndangeredAnimal) {
-
+        try(Connection con = sql2o.open()){
+            String sql = "UPDATE animals SET name = :name WHERE id = :id";
+            con.createQuery(sql)
+                    .addParameter("name", nonEndangeredAnimal.name)
+                    .executeUpdate();
+        }
     }
+
+    //findsAll
 
     @Override
     public NonEndangeredAnimal getNonEndangeredAnimalById(int id) {
-        return null;
-    }
+            try (Connection con = DB.sql2o.open()) {
+                String sql = "SELECT * FROM animals where id=:id";
+                NonEndangeredAnimal nonendangeredAnimal = con.createQuery(sql)
+                        .addParameter("id", id)
+                        .executeAndFetchFirst(NonEndangeredAnimal.class);
+                return nonendangeredAnimal;
 
+            }
+        }
+
+    //READ
     @Override
     public List<NonEndangeredAnimal> getNonEndangeredAnimal() {
-        return null;
+        try(Connection con = sql2o.open()){
+            String sql = "SELECT * FROM animals";
+            return con.createQuery(sql).executeAndFetch(NonEndangeredAnimal.class);
+        }
     }
-
+    //DELETE
     @Override
     public void deleteNonEndangeredAnimal(int id) {
+        try (Connection con = sql2o.open()){
+            String sql = "DELETE FROM animals WHERE id =:id";
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
 
     }
 }
--- src/test/java/models/NonEndangeredAnimalTest.java
+++ src/test/java/models/NonEndangeredAnimalTest.java
@@ -1,15 +1,25 @@
 package models;
 
+import DAO.EndangeredAnimalDao;
+import DAO.NonEndangeredAnimalDao;
 import models.EndangeredAnimal;
+import org.junit.Rule;
 import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class NonEndangeredAnimalTest {
 
-//    @Test
-//    public void all_returnsAllInstancesOfArr_true() {
-//        EndangeredAnimal endangeredAnimal = new EndangeredAnimal("Animal");
-//        assertEquals("Animal", endangeredAnimal.getName());
-//    }
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+
+    @Test
+    public void save_insertsObjectIntoDatabase_true() {
+        NonEndangeredAnimal nonendangeredAnimal = new NonEndangeredAnimal("rhino");
+        NonEndangeredAnimalDao nonendangeredAnimalDao = new NonEndangeredAnimalDao();
+        nonendangeredAnimalDao.add(nonendangeredAnimal);
+        assertTrue(nonendangeredAnimalDao.getNonEndangeredAnimal().get(0).equals(nonendangeredAnimal));
+    }
 }
