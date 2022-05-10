0153ee933ae7c712a1c564923ee5cc7c2c65e234
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-05-10 11:47:53 +0300

    This will add the sightings tests

--- src/test/java/models/SightingsTest.java
+++ src/test/java/models/SightingsTest.java
@@ -1,5 +1,7 @@
 package models;
 
+import DAO.EndangeredAnimalDao;
+import DAO.NonEndangeredAnimalDao;
 import DAO.SightingDao;
 import org.junit.Rule;
 import org.junit.Test;
@@ -9,12 +11,28 @@ import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SightingTest {
+public class SightingsTest {
     @Rule
     public DatabaseRule database = new DatabaseRule();
 //
-
-
+@Test
+public void Sighting_instantiatesgetName_true() {
+    Sighting sighting = new Sighting("LION","ZONE A","AFRED");
+    SightingDao sightingDao = new SightingDao();
+    assertEquals("LION", sighting.getName());
+}
+    @Test
+    public void Sighting_instantiatesgetLocation_true() {
+        Sighting sighting = new Sighting("LION","ZONE A","AFRED");
+        SightingDao sightingDao = new SightingDao();
+        assertEquals("ZONE A", sighting.getLocation());
+    }
+    @Test
+    public void Sighting_instantiatesgetStrangerName_true() {
+        Sighting sighting = new Sighting("LION","ZONE A","AFRED");
+        SightingDao sightingDao = new SightingDao();
+        assertEquals("AFRED", sighting.getRanger());
+    }
 
 
 
